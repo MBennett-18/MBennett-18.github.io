56a24e74efbe3ec846a1271f9f7840fe5fda1030
MBennett-18 <mark.bennett@dal.ca>
2022-08-10 19:15:29 -0300

    Add tabbed layout

---
+++ app.js
@@ -0,0 +1,23 @@
+document.getElementById("defaultLand").click();
+
+function openTab(evt, tabName) {
+    // Declare all variables
+    let tabcontent; 
+    let tablinks;
+  
+    // Get all elements with class="content" and hide them
+    tabcontent = document.getElementsByClassName("content");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tab-link");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    // Show the current tab, and add an "active" class to the link that opened the tab
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
