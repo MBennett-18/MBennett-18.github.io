478e604ff8920704a60d724a23e22df2aabb0a5c
MBennett-18 <mark.bennett@dal.ca>
2022-09-14 16:26:05 -0300

    Switch back fetch

--- js/nd-reporting.js
+++ js/nd-reporting.js
@@ -1,53 +1,33 @@
 
 
-function getvals(){
-  return fetch('https://jsonplaceholder.typicode.com/posts')
-    .then((response) => response.json());
-}
+const apiCounts = "https://data.novascotia.ca/resource/mdfn-jkdg.json";
+fetch(apiCounts)
+.then(apiResponse => apiResponse.json())
+.then(apiData => { 
+    const filteredData = apiData.filter(function (el) {
+        return el.disease === "Chlamydia";
+      });
+    const dataSortable = filteredData.map(Object.values);
 
-const data = await getvals(); // Waits here until getvals() is done.
-console.log(data);
+    dataSortable.sort(function(a,b) {
+        return a[0] - b[0];
+    });
 
-    // const seriesName = dataSortable[0][1]
-    // // create a chart
-    // const data = anychart.data.set(dataSortable)
-    // var seriesData = data.mapAs({x: 0, value: 2});
-    // var chart = anychart.line();
-    // var series = chart.line(seriesData);
-    
-    // // set the titles of the axes
-    // // set the container id
-    // chart.container("annualCounts-id");
+// const seriesName = dataSortable[0][1]
+// create a chart
+const data = anychart.data.set(dataSortable)
+var seriesData = data.mapAs({x: 0, value: 2});
+var chart = anychart.line();
+var series = chart.line(seriesData);
 
-    // // initiate drawing the chart
-    // chart.draw();
+// set the titles of the axes
+// set the container id
+chart.container("annualCounts-id");
 
-    // function fetchIt() {
-//     const apiCounts = "https://data.novascotia.ca/resource/mdfn-jkdg.json";
-//     fetch(apiCounts)
-//     .then(apiResponse => apiResponse.json())
-//     .then(apiData => { 
-//         const filteredData = apiData.filter(function (el) {
-//             return el.disease === "Chlamydia";
-//           });
-//         const dataSortable = filteredData.map(Object.values);
-
-//         dataSortable.sort(function(a,b) {
-//             return a[0] - b[0];
-//         });
-
-//         return dataSortable;  
-//     });
-// }
+// initiate drawing the chart
+chart.draw();
+});
 
 
 
 
-// function getvals(){
-//   return fetch('https://jsonplaceholder.typicode.com/posts')
-//   //return fetch('https://data.novascotia.ca/resource/mdfn-jkdg.json')
-//   .then((response) => response.json())
-//   .then((responseData) => {
-//     return responseData;
-//   })
-// }
